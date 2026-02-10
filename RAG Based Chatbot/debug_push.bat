4da504abd1b810baed1e3ac46ee9143d113e2168
Sai-Pallav <kotasaipallav123@gmail.com>
2026-02-10 09:47:37 +0530

    Remove temporary debug scripts

--- RAG Based Chatbot/debug_push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd ..
-echo STARTING PUSH... > "RAG Based Chatbot\git_push_log.txt"
-git push origin main >> "RAG Based Chatbot\git_push_log.txt" 2>&1
-echo FINISHED. >> "RAG Based Chatbot\git_push_log.txt"
